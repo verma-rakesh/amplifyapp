328a3e0e9edca4720ede1e00ec69014c97d60498
Rakesh Verma <rakesh.verma@trilogy.com>
2021-04-28 08:46:38 -0700

    initial commit V4

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello Rakesh Verma from V3</h1>
+        <h1>Hello Rakesh Verma from V4</h1>
       </header>
     </div>
   );
